89fe3a203f22b486763bf409bba6d1dab7465013
Guido Pontet <gpontet@fu.do>
2024-01-02 11:20:51 -0300

    feat: persist favourite repos to local storage middleware

--- src/store/favouriteRepos.ts
+++ src/store/favouriteRepos.ts
@@ -1,17 +1,25 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 import { FavouriteReposState } from "../interfaces/favouriteReposState";
 
-export const useFavouriteReposStore = create<FavouriteReposState>((set) => ({
-  favouriteReposIds: [],
-  addFavouriteRepo: (id: number) => {
-    set((state) => ({
-      favouriteReposIds: [...state.favouriteReposIds, id],
-    }));
-  },
-  removeFavouriteRepo: (id: number) => {
-    set((state) => ({
-      favouriteReposIds: state.favouriteReposIds.filter((repoId) => repoId !== id),
-    }));
-  },
-}));
+export const useFavouriteReposStore = create(
+  persist<FavouriteReposState>(
+    (set) => ({
+      favouriteReposIds: [],
+      addFavouriteRepo: (id: number) => {
+        set((state) => ({
+          favouriteReposIds: [...state.favouriteReposIds, id],
+        }));
+      },
+      removeFavouriteRepo: (id: number) => {
+        set((state) => ({
+          favouriteReposIds: state.favouriteReposIds.filter((repoId) => repoId !== id),
+        }));
+      },
+    }),
+    {
+      name: "favourite-repos",
+    }
+  )
+);
